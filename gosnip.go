923b3447068e11ea7785cad23d2edac129fc966c
JerBerlin <jeronir@hotmail.com>
2023-07-08 15:59:44 +0700

    correct case in comments

--- gosnip.go
+++ gosnip.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-// spinner spins for the specified duration, printing a spinner character to the terminal every 50 milliseconds.
+// Spinner spins for the specified duration, printing a spinner character to the terminal every 50 milliseconds.
 func Spinner(duration time.Duration) {
 	endTime := time.Now().Add(duration)
 	for time.Now().Before(endTime) {
@@ -20,14 +20,14 @@ func Spinner(duration time.Duration) {
 	fmt.Printf("\r ")
 }
 
-// clearScreen clears the terminal screen.
+// ClearScreen clears the terminal screen.
 func ClearScreen() {
 	cmd := exec.Command("clear")
 	cmd.Stdout = os.Stdout
 	cmd.Run()
 }
 
-// bPrintln prints a beautified message with optional emojis.
+// BPrintln prints a beautified message with optional emojis.
 // It prints the emojis (if any), followed by the message, with a newline before and after, and the text in bright blue.
 // The emojis are passed as a variable number of rune arguments, and they are printed before the message.
 // A space is added between the emojis and the text for better readability.
